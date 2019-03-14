b6a5d55bf32e13ab643a140b204a46e4a0c9912a
Nacho Garcia Cano <igarcia@beta07.aulas.gsyc.urjc.es>
2019-03-14 09:42:55 +0100

    pong

--- pong.js
+++ pong.js
@@ -8,6 +8,31 @@ function main()
 
   var ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = 'white';
-  ctx.fillRect(50,30, 10, 40)
+  var bola = {
+    x_ini: 50;
+    y_ini: 50;
+
+    x: 0;
+    y: 0;
+
+    v_x: 4;
+    v_y: 1;
+
+    b_width: 5;
+    b_height: 5;
+
+    ctx: null;
+
+    draw: function(){
+      this.ctx.fillStyle = white;
+      this.ctx.fillRect(this.x,this.y,this.b_width, this.b_height);
+    };
+
+    ini: function(){
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+    };
+
+  }
+
 }
